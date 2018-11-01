203b9676964496b80cd9df5266864e971f719f0c
Anton Breslavsky <breslavsky.anton@gmail.com>
2018-11-01 22:15:35 +0300

    first init

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+//https://plot.ly/~notebook_demo/84.embed
+//https://plot.ly/create/
+//https://github.com/plotly/angular-plotly.js/blob/master/README.md
+
+const DATA_LINK = 'https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  data = [];
+  layout = {
+    title: 'Contours projection of surface',
+    autosize: true
+  };
+
+  constructor(@Inject(HttpClient) private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get(DATA_LINK, {responseType: 'text'})
+      .subscribe(csv => this.draw(csv));
+
+  }
+
+  private draw(csv: string) {
+    this.data = [
+      {
+        z: this.getData(csv),
+        type: 'surface',
+        contours: {
+          z: {
+            show: true,
+            usecolormap: true,
+            highlightcolor: "#42f462",
+            width: 10,
+            project: {z: true}
+          },
+          x: {
+            show: true,
+            usecolormap: true,
+            highlightcolor: "#42f462",
+            project: {x: true}
+          },
+          y: {
+            show: true,
+            usecolormap: true,
+            highlightcolor: "#42f462",
+            project: {y: true}
+          }
+        }
+      },
+    ];
+  }
+
+  private getData(csv: string) {
+    let rows = csv.split("\n");
+    let data = [];
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i].split(',');
+      data.push(row.map(c => c.trim()));
+    }
+    return data;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PlotlyModule} from "angular-plotly.js";
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PlotlyModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
